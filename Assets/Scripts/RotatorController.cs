0c328e6cc195c72252bad01b16274578c46678e1
Luiz Guilherme Bandeira <arkanael@gmail.com>
2023-06-17 00:45:36 -0300

    Adicionar arquivos de projeto.

---
+++ Assets/Scripts/RotatorController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatorController : MonoBehaviour
+{
+    public float speedRotator = 100f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0f, 0f, speedRotator * Time.deltaTime);
+    }
+}
